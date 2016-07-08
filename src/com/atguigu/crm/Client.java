e009048c818856904c46e55db3521690f773c57f
liuyiming164 <455097023@qq.com>
2016-07-08 21:45:06 +0800

    ccc

--- src/com/atguigu/crm/Client.java
+++ src/com/atguigu/crm/Client.java
@@ -6,7 +6,6 @@ public class Client
 	public static void main(String[] args)
 	{
 		System.out.println("welcome......");
-		System.out.println("1111111111111");
 		System.out.println("2222222222");
 	}
 
